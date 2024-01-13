3971d469044fde436e25ff406908e5e02bffba69
Ritwik880 <rittiksinha12@gmail.com>
2024-01-13 16:05:16 +0530

    Fix in timeline

--- src/Components/Timeline.jsx
+++ src/Components/Timeline.jsx
@@ -2,42 +2,29 @@
 
 import React from 'react';
 import { EVENTSDATA as data } from '../constants/utils';
+import '../Timeline.css';
+
 const Timeline = () => {
-
-
   return (
     <section className='timeline-section'>
       <div className='row container'>
-        <div>
+        <div className='timeline-content-div'>
           <h1 className='timeline-heading'>ROAD MAP</h1>
           <p className='timeline-para'>
             A product roadmap shows the path ahead, helps teams plan,
             and guides the delivery of the product.
-
           </p>
         </div>
-        <div>
-          {
-            data && data.map((item, id) => {
-              return (
-                <div key={id}>
-                  <div class="timeline-empty">
-                  </div>
-                  <div class="timeline-middle">
-                    <div class="timeline-circle"></div>
-                  </div>
-                  <div class="timeline-component timeline-content">
-                    <h3>
-                      {item.name}
-                    </h3>
-                    <p>
-                      {item.description}
-                    </p>
-                  </div>
-                </div>
-              )
-            })
-          }
+        <div className="timeline">
+          {data.map((event, index) => (
+            <div key={index} className={`timeline-container ${index % 2 === 0 ? 'left' : 'right'}`}>
+              {index === 0 && <div className='timeline-point'></div>}
+              <div className="content">
+                <h2 className='timeline-card-heading'>{event.name}</h2>
+                <p className='timeline-card-para'>{event.description}</p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
