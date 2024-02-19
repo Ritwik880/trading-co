08d3996061bb79f8e8d2b2963504ee5092234f71
Ritwik880 <rittiksinha12@gmail.com>
2024-02-19 22:32:14 +0530

    fix

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,21 +1,78 @@
 import React from 'react'
+import '../Contact.css';
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 const Contact = () => {
     return (
         <>
-            <section className='contact-section'></section>
-            <section className='contact-section-div'>
+            <section className='contact'>
                 <div className='row container'>
-                    <div className='contact-section-parent'>
-                        <div className='contact-section-left-box'>
-                            <span className='contact-section-span'>
-                                COMPANY ADDRESS
-                            </span>
-                            <h3 className='contact-heading'>
-                                202, 2 Floor Vijay, Laxmienclavekaviramanpath, Navshakti, Patna, Phulwari, Bihar, India, 800001
-                            </h3>
+                    <h1 className='about-heading'>
+                        Contacts
+                    </h1>
+                    <div className='about-navigation-div'>
+                        <a href='/' className='about-link'>Home</a>
+                        <span className='about-link'>
+                            /
+                        </span>
+                        <a href='/about' className='about-link'>
+                            Contacts
+                        </a>
+                    </div>
+                </div>
+            </section>
+
+            <section className='about-part4'>
+                <div className='row container'>
+                    <div className='col-lg-12 col-md-12'>
+                        <div className="row about-input-div">
+                            <div className="col">
+                                <input type="text" className="form-control contact-input-box" placeholder="Your Full Name" aria-label="First name" />
+                            </div>
+                            <div className="col">
+                                <input type="text" className="form-control contact-input-box" placeholder="Your Email Address" aria-label="Last name" />
+                            </div>
                         </div>
-                        <div className='contact-social-parent-div'>
+                        <div className="row about-input-div">
+                            <div className="col">
+                                <input type="text" className="form-control contact-input-box" placeholder="Your Phone Number" aria-label="First name" />
+                            </div>
+                            <div className="col">
+                                <input type="text" className="form-control contact-input-box" placeholder="Your Address" aria-label="Last name" />
+                            </div>
+                        </div>
+                        <div>
+                            <textarea className="form-control contact-input-box" placeholder="Enter Your Message Here..." rows={6} onResize={false}></textarea>
+                        </div>
+                        <div className='d-flex justify-content-start mt-4'>
+                            <button className='learn-more-btn'>
+                                Submit Message <FaLongArrowAltRight />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className='d-flex justify-content-center align-items-center'>
+                <div className='row container'>
+                    <img src='/contact-map.svg' alt='map'/>
+                </div>
+            </section>
+
+            <section className='d-flex justify-content-center align-items-center contact-info'>
+                <div className='row container'>
+                    <div className='col-lg-5 col-md-12'>
+                        <div className='info-div'>
+                            <span className='info-span'>
+                            Company Address
+                            </span>
+                            <p className='info-para'>
+                            202, 2 Floor Vijay, Laxmienclavekaviramanpath, Navshakti, Patna, Phulwari, Bihar, India, 800001
+                            </p>
+                        </div>
+                    </div>
+                    <div className='col-lg-7 col-md-12'>
+                    <div className='contact-social-parent-div'>
                             <div className='contact-social-div'>
                                 <img src='/call.svg' alt='call' />
                                 <div>
---
+++ src/Components/Mining.jsx
@@ -0,0 +1,131 @@
+import React from 'react'
+import '../Mining.css';
+import { MINEIMAGES as images } from '../constants/utils';
+import { MININGDIV as div } from '../constants/utils';
+import { MININGPARA as para } from '../constants/utils';
+import { SERVICES as service } from '../constants/utils';
+import { FaLongArrowAltRight } from "react-icons/fa";
+
+const Mining = () => {
+  return (
+    <>
+      <section className='mining'>
+        <div className='row container'>
+          <h1 className='about-heading'>
+            Stone Mining
+          </h1>
+          <div className='about-navigation-div'>
+            <a href='/' className='about-link'>Home</a>
+            <span className='about-link'>
+              /
+            </span>
+            <a href='/about' className='about-link'>
+              Mining
+            </a>
+            <span className='about-link'>
+              /
+            </span>
+            <a href='/about' className='about-link'>
+              Stone Mining
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <div className='mining-social-parent'>
+        <div className='mining-social'>
+          <span className='mining-span'>
+            Call Us Today
+          </span>
+          <p className='mining-phone'>
+            + 91 942 355 4569
+          </p>
+        </div>
+      </div>
+
+      <section className='d-flex justify-content-center align-items-center mining-landing'>
+        <div className='row container'>
+          <div className='mine-image-div'>
+            {
+              images.map((item, index) => (
+                <img src={item.img} key={index} className='mine-image' />
+              ))
+            }
+          </div>
+        </div>
+      </section>
+
+      <section className='d-flex justify-content-center align-items-center about-mining'>
+        <div className='row container'>
+          <div className='col-lg-7 col-md-12'>
+            <h6 className='mining-heading'>
+              Mining
+            </h6>
+            {
+              para.map((item, index) => (
+                <p className='mining-main-para' key={index}>
+                  {
+                    item.para
+                  }
+                </p>
+              ))
+            }
+          </div>
+          <div className='col-lg-5 col-md-12'>
+            {
+              div.map((item, index) => (
+                <div key={index} className={`mining-info ${index % 2 === 0 ? 'even-background' : 'odd-background'}`}>
+                  <h6 className='mining-text'>
+                    {item.name}
+                  </h6>
+                  {
+                    item.title && <p className='mining-para'>
+                      {item.title}
+                    </p>
+                  }
+                  <button className='mining-btn-arrow'>
+                    <FaLongArrowAltRight fontSize={30} />
+                  </button>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </section>
+
+      <section className='d-flex justify-content-center align-items-center mining-service'>
+        <div className='row container'>
+          <h1 className='service-heading'>
+            Other Services
+          </h1>
+          <div className='service-parent-div'>
+            {
+              service.map((item, index) => (
+                <div key={index} className='service-div'>
+                  <span>
+                    {
+                      item.title
+                    }
+                  </span>
+                  <p>
+                    {
+                      item.desc
+                    }
+                  </p>
+                  <button className='service-btn'>
+                    {
+                      item.btn
+                    }
+                    <FaLongArrowAltRight fontSize={20} className='mx-2' />
+                  </button>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Mining
